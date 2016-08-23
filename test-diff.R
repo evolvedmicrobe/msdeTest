189d2acd23d67d2d40e19251700cf5776e79a52a
mlysy <mlysy@users.noreply.github.com>
2016-08-23 00:09:25 -0400

    Updated functions

--- test-diff.R
+++ test-diff.R
@@ -1,4 +1,4 @@
-#--- check that two version of same package produce identical results -----------
+#--- check that two versions of same package produce identical results -----------
 
 pkg1 <- "msdeTest"
 pkg2 <- "msdeTest2"
@@ -15,6 +15,7 @@ ndims <- 2
 nparams <- 5
 param.names <- c("alpha", "gamma", "beta", "sigma", "rho")
 data.names <- c("X", "Z")
+
 hmod <- parsePkg(pkg1, "sde.make.model2")
 hmod <- hmod(code = file.path(msdeTest:::.msdeCppPath,
                "hestModel.cpp"),
@@ -37,7 +38,6 @@ x0 <- c(X = log(1000), Z = 0.1)
 nReps <- 10
 Theta <- apply(t(replicate(nReps, theta)), 2, jitter)
 X0 <- apply(t(replicate(nReps, x0)), 2, jitter)
-
 
 dr <- parsePkg(pkg1, "sde.drift2")(model = hmod, x = X0, theta = Theta)
 df <- parsePkg(pkg1, "sde.diff2")(model = hmod, x = X0, theta = Theta)
@@ -78,3 +78,53 @@ identical(hsim, hsim2)
 ll1 <- parsePkg(pkg1, "sde.loglik2")
 ll1 <- ll1(model = hmod, x = hsim$data, dt = dT,
            theta = hsim$params)
+
+ll2 <- parsePkg(pkg2, "sde.loglik")
+ll2 <- ll2(model = hmod2, x = hsim$data, dt = dT,
+           theta = hsim$params)
+
+range(ll1-ll2)
+
+#--- posterior sampling ---------------------------------------------------------
+
+nObs <- 250
+dT <- 1/252
+hsim <- msdeTest::sde.sim2(model = hmod, init.data = x0,
+                           params = theta,
+                           dt = dT, dt.sim = dT/100, N = nObs, nreps = 1)
+
+# initialize MCMC rv's
+init <- msdeTest2::sde.init(data = hsim$data, dt = dT, m = 2,
+                            par.index = c(2, rep(nObs-1, 1)),
+                            params = theta)
+
+# prior
+prior <- list(Mu = c(.1, .35, 1.0, .5, -.81),
+              V = crossprod(matrix(rnorm(25),5)))
+prior$V <- sqrt(diag(c(.1, 8, .15, .002, .002))) %*% cov2cor(prior$V)
+prior$V <- prior$V %*% sqrt(diag(c(.1, 8, .15, .002, .002)))
+
+# mcmc specs
+nsamples <- 1e3
+burn <- 1e2
+rw.jump.sd <- c(.1, 1, .1, .01, .01) # random walk metropolis for params
+update.params <- TRUE
+update.data <- TRUE
+
+set.seed(531)
+hpost <- parsePkg(pkg1, "sde.post2")
+hpost <- hpost(model = hmod, init = init,
+               nsamples = nsamples, burn = burn, prior = prior,
+               rw.jump.sd = rw.jump.sd,
+               update.params = update.params,
+               update.data = update.data)
+
+set.seed(531)
+hpost2 <- parsePkg(pkg2, "sde.post")
+hpost2 <- hpost2(model = hmod2, init = init,
+                 nsamples = nsamples, burn = burn, prior = prior,
+                 rw.jump.sd = rw.jump.sd,
+                 update.params = update.params,
+                 update.data = update.data)
+
+identical(hpost, hpost2)
